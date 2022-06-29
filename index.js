af0a0607893cbb16cdf9afec3f1be9a527ca3b16
Valentin <vainferro@gmail.com>
2022-06-30 00:26:36 +0300

    Added block to show when element is selected

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+// Variables
 
-function computerPlay() {
-let randomHand = Math.floor(Math.random()*4)+1
-}
-    
+const hand = document.getElementById("hand")
+let rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissor = document.getElementById("scissor")
+let resultEL = document.getElementById("results-container")
+let elementsEl = document.getElementById("elementsEl")
+const random = document.getElementById("random")
 
+
+
+rock.addEventListener('click', function() {
+    resultEL.innerHTML = `<img class="showRock" src="/images/rock.png">`
+    elementsEl.style.display = 'none'
+})
+
+
+
+
+let randomRoll = Math.floor(Math.random()*3)+1
+console.log(randomRoll)
 
